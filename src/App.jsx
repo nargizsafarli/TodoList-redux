2268a6d8f0987d0deaa7915a00f8e4ae617c8896
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-03-04 22:31:30 +0400

    Animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import TodoList from './Components/TodoList/TodoList'
 import TodoForm from './Components/TodoForm/TodoForm'
-import logo from "./assets/logo-34.png"
+import logo from "./assets/bg34-1.png"
 
 function App() {
   return (
     <div className='main-container'>
-    {/* <img className='logo' src={logo}/> */}
+    <img className='logo' src={logo}/>
     <div className='text'>
     <h3>Sign In to Rechage Direct</h3>
     <p>If you don’t have an account You can Sign Up</p>
